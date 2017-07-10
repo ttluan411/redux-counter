35a812e80b099492d9b83d477837337444b55489
Logan Truong <ttluan411@gmail.com>
2017-07-10 14:52:10 -0600

    done

--- src/App.js
+++ src/App.js
@@ -1,50 +1,63 @@
 import React, { Component } from "react";
-
+import { connect } from 'react-redux';
 import "./App.css";
-
+import {increment, decrement,redo, undo} from './ducks/counter';
 export class App extends Component {
+
+	
+
+
 	render() {
+		const {
+			currentValue,
+			decrement,
+			increment,
+			previousValues,
+			futureValues,
+			redo,
+			undo
+		} = this.props;
 		return (
 			<div className="app">
 				<section className="counter">
-					<h1 className="counter__current-value">{ 0 }</h1>
+					<h1 className="counter__current-value">{ currentValue }</h1>
 					<div className="counter__button-wrapper">
 						<button
 							className="counter__button increment-one"
-							onClick={ () => null }
+							onClick={ () => increment( 1 ) }
 						>
 							+1
 						</button>
 						<button
 							className="counter__button increment-five"
-							onClick={ () => null }
+							onClick={ () => increment( 5 ) }
 						>
 							+5
 						</button>
 						<button
 							className="counter__button decrement-one"
-							onClick={ () => null }
+							onClick={ () => decrement( 1 ) }
 						>
 							-1
 						</button>
 						<button
 							className="counter__button decrement-five"
-							onClick={ () => null }
+							onClick={ () => decrement( 5 ) }
 						>
 							-5
 						</button>
 						<br />
 						<button
 							className="counter__button undo"
-							disabled={ true }
-							onClick={ () => null }
+							disabled={ previousValues.length === 0 }
+							onClick={ undo }
 						>
 							Undo
 						</button>
 						<button
 							className="counter__button redo"
-							disabled={ true }
-							onClick={ () => null }
+							disabled={ futureValues.length === 0 }
+							onClick={ redo }
 						>
 							Redo
 						</button>
@@ -60,4 +73,7 @@ export class App extends Component {
 	}
 }
 
-export default App;
+function mapStateToProps (state){
+	return state
+}
+export default connect( mapStateToProps, {decrement, increment, redo , undo} )(App);
---
+++ src/store.js
@@ -0,0 +1,3 @@
+import { createStore }  from 'redux';
+import counter from './ducks/counter';
+export default createStore(counter);
